1fd679e009ce34c3c03172cb8421ef1875bdb35c
Markus Mayer <widemeadows@gmail.com>
2016-07-14 01:57:05 +0200

    added workaround for the missing __anon symbol

--- include/KaleidoscopeJIT.h
+++ include/KaleidoscopeJIT.h
@@ -104,7 +104,7 @@ namespace llvm {
                 // This is the opposite of the usual search order for dlsym, but makes more
                 // sense in a REPL where we want to bind to the newest available definition.
                 for (auto H : make_range(ModuleHandles.rbegin(), ModuleHandles.rend()))
-                    if (auto Sym = CompileLayer.findSymbolIn(H, Name, true))
+                    if (auto Sym = CompileLayer.findSymbolIn(H, Name, false /* <-- http://stackoverflow.com/a/33717957/195651 */))
                         return Sym;
 
                 // If we can't find the symbol in the JIT, try looking in the host process.
